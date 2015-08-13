8e0d6b096f98e1afb72fc6f92e1709312672ad06
Alex Rodriguez <al3xelias@gmail.com>
2015-08-13 02:29:27 -0400

    Add load script for platform

--- sprites/loadSprites.js
+++ sprites/loadSprites.js
@@ -1,4 +1,5 @@
 game.load.script('computer.js', 'sprites/computer.js');
 game.load.script('bluevirus.js', 'sprites/bluevirus.js');
 game.load.script('redvirus.js', 'sprites/redvirus.js');
-game.load.script('computerCollision.js','sprites/computerCollision.js')
+game.load.script('computerCollision.js','sprites/computerCollision.js');
+game.load.script('platform.js','sprites/platform.js')
