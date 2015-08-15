12c1884df90cee5b70373e12efe06204ad8c6360
Alex Rodriguez <al3xelias@gmail.com>
2015-08-15 02:00:55 -0400

    Set boundary when dragging viruses -Create gameCanvas sprite -Only set for blueViruses at moment

--- sprites/createStage.js
+++ sprites/createStage.js
@@ -1,14 +1,23 @@
 var createStage = function(that){
 
-  	var bg = that.game.add.sprite(0,0,'background');
-	bg.width = 800;
-	bg.height = 600;
+  bg = that.game.add.sprite(0,0,'background');
+  bg.width = 800;
+  bg.height = 600;
+
+  gameCanvas = that.game.add.sprite(0,-5, 'ground');
+  gameCanvas.width = 800;
+  gameCanvas.height = 500;
 
 	ground = that.game.add.sprite(0,500, 'ground');
-	ground.width = 800
-	ground.height = 180
+	ground.width = 800;
+	ground.height = 180;
 
 	that.game.physics.enable(ground, Phaser.Physics.ARCADE);
+
+  that.game.physics.enable(gameCanvas, Phaser.Physics.ARCADE);
+
 	ground.body.immovable = true;
+  
+  gameCanvas.body.immovable = true;
 
 }
