26082f59d22c9fb0f5d16babcfe459ff55b5fb41
Ken Kim <alteryush@gmail.com>
2015-08-15 17:54:48 -0700

    xlogz changes

--- towerscum.js
+++ towerscum.js
@@ -1,10 +1,12 @@
 var towerScum = function(game){}
-var health = 128; //set it to whatever value this.barProgress equal to.
-
+var health = 128;
+var redViruses = {};
+var blueViruses = {};
+var yellowViruses = {};
  var attack = function(virus){
  	console.log('attacking');
     virus.animations.play('attack');
-    health--; //health decrement
+    health--;
     virus.y = virus.y - 25;
   }
 
@@ -13,26 +15,101 @@ towerScum.prototype = {
     var result = number + (number * 0.5 );
     return result;
   },
+  endRound: function(){
+  	if(redViruses.hasOwnProperty('children')){
+  		var totalLeft = blueViruses.children.length + redViruses.children.length;
+  	}else{
+
+  		var totalLeft = blueViruses.children.length;
+  		console.log('total left: ', totalLeft)
+  	}
+  	
+  	if(!totalLeft ){
+  		console.log('round ended')
+  		alert('end of round!');
+  		this.roundStarted = false;
+  		this.round++;
+  		this.rounds[this.round](this);
+  	}
+  },
+   rounds : {
+	1: function(context){
+	    blueVirus(context, 0, 0, 5)
+		},
+	2: function(context){
+	    blueVirus(context, 0, 0, 7)
+		},
+	3: function(context){
+	    blueVirus(context, 0, 0, 10)
+	    redVirus(context, 0, 0, 1)
+		},
+	4: function(context){
+	    blueVirus(context, 0, 0, 15)
+	    redVirus(context, 0, 0, 3)
+		},
+	5: function(context){
+	    blueVirus(context, 0, 0, 15)
+	    redVirus(context, 0, 0, 5)
+		},
+	6: function(context){
+	    blueVirus(context, 0, 0, 20)
+	    redVirus(context, 0, 0, 5)
+		},
+	7: function(context){
+	    blueVirus(context, 0, 0, 25)
+	    redVirus(context, 0, 0, 5)
+		},
+	8: function(context){
+	    blueVirus(context, 0, 0, 25)
+	    redVirus(context, 0, 0, 10)
+		},
+	9: function(context){
+	    blueVirus(context, 0, 0, 30)
+	    redVirus(context, 0, 0, 10)
+		},
+	10: function(context){
+	    blueVirus(context, 0, 0, 35)
+	    redVirus(context, 0, 0, 15)
+		},
+	11: function(context){
+	    blueVirus(context, 0, 0, 40)
+	    redVirus(context, 0, 0, 20)
+		},
+	12: function(context){
+	    blueVirus(context, 0, 0, 45)
+	    redVirus(context, 0, 0, 20)
+		},
+	13: function(context){
+	    blueVirus(context, 0, 0, 50)
+	    redVirus(context, 0, 0, 25)
+		},
+	context: function(context){
+		console.log(context);
+	}
+
+
+},
+	round: 1,
+
 
   create: function() {
   	console.log('Creating game...');
 
   	createStage(this);
 
-
     compSprite(this); //takes x and y coordinates for positioning
     computerCollision(this);
 
-    blueVirus(this, 0, 0, 5)
-    redVirus(this, 0, 0, 3)
- 
+    //blueVirus(this, 0, 0, 5)
+    //redVirus(this, 0, 0, 3)
+
+    this.rounds[this.round](this);
+
     this.outerbar = this.add.bitmapData(134, 13);
     this.outerbar.context.fillStyle = '#00685e';
     this.outerbar.fill()
-   
-    this.barProgress = 128;//this is the health setter this.barProgress = 300 means 300 health. 
+    this.barProgress = 128;      
     this.bar = this.add.bitmapData(128, 10); //sets width and height for bar
-    
     this.game.add.sprite(633, 298, this.outerbar); //outerbar
     this.game.add.sprite(700 - (this.bar.width * 0.5), 300, this.bar);//x and y coordinates of bar 700,300
 
@@ -69,7 +146,19 @@ towerScum.prototype = {
         
     this.bar.context.fillRect(0, 0, this.barProgress, 8);
     this.game.add.tween(this).to({barProgress: health}, 100, null, true, 0); //each hit health updates and bar shrinks
+
     this.bar.dirty = true; //apparently this line is important but I dont know why
+
+    if(blueViruses.children.length){
+  		this.roundStarted = true;
+  	}
+
+  	if(this.roundStarted){
+  		this.endRound();
+  	}
+
+
+
   },
 
   render: function(){
