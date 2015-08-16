180292d76d0e1e51282894f63f6e60d0611f7fea
Ajora <alteryush@gmail.com>
2015-08-16 07:00:49 -0700

    comments

--- sprites/createStage.js
+++ sprites/createStage.js
@@ -6,11 +6,11 @@ var createStage = function(that){
 
   gameCanvas = that.game.add.sprite(0, 0, 'background');
   gameCanvas.width = 800;
-  gameCanvas.height = 502;
+  gameCanvas.height = 502; //made this 502 to prevent the white lines from showing
 
   ground = that.game.add.sprite(0,500, 'ground');
   ground.width = 800;
-  ground.height = 178;
+  ground.height = 178; //as bg.height increases lower this number by whatever you increase bg by
 
   that.game.physics.enable(ground, Phaser.Physics.ARCADE);
 
